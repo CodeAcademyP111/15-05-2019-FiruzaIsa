3d1bc652166dee4f2908c7872234d245c9fdd8d6
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-05-15 17:01:41 +0400

    Add files via upload

---
+++ sidebar/bar.js
@@ -0,0 +1,11 @@
+function clicktochange(){
+document.querySelector(".sidebar").style.marginLeft="0px";
+document.querySelector(".sidebar").style.transition="1s";
+
+}
+function clicktoclose(){
+    document.querySelector(".sidebar").style.marginLeft="-300px";
+}
+
+
+
